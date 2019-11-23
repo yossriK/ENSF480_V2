0d13e02bef72c8565eb4f2a55404fd016c102580
yossriK <46327024+yossriK@users.noreply.github.com>
2019-11-23 00:58:29 -0700

    Update Renter.java

--- server/model/Renter.java
+++ server/model/Renter.java
@@ -1,6 +1,7 @@
 package server.model;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -41,39 +42,57 @@ public class Renter extends Person{
 	
 	
 	public String Search(String recieved) throws SQLException {
-		Statement myStmt= myConnection.createStatement();
+		PreparedStatement myStmt= myConnection.prepareStatement("SELECT * FROM properties where propertyType LIKE ? AND nomBedroom LIKE ? AND nomBathroom LIKE ? AND furnished LIKE ? AND cityQuadrent LIKE ?");
+		
 		String sendBack="";
 		String [] temp=recieved.split(","); // 1 propertytype, 2 bedrom, 3bath, 4 furnish, 5cityquad, 
-		String query="SELECT * FROM properties WHERE ";
+	
 		if(temp[1]!="*") {
-			query+="propertyType="+temp[1]+" AND ";
+			myStmt.setString(1, "%"+temp[1]+"%");
 		}
+		else
+			myStmt.setString(1, "%");
 		if(temp[2]!="*") {
-			query+="nomBedroom="+temp[2]+" AND ";
-		}
-		if(temp[3]!="*") {
-			query+="nomBathroom="+temp[3]+" AND ";
-		}
-		if(temp[4]!="*") {
-			query+="furnished="+temp[4]+" AND ";
-		}
-		if(temp[5]!="*") {
-			query+="cityQuadrent="+temp[5];
-		}
-
+			myStmt.setString(2, temp[2]);
 		
-		ResultSet myRs = myStmt.executeQuery(query);
+		}
+		else		
+			myStmt.setString(2, "%");
+
+
+		if(temp[3].compareTo("*")!=0) {
+			myStmt.setString(3, temp[3]);
+		}
+		else
+			myStmt.setString(3, "%");
+
+		if(temp[4]!="*") {
+			myStmt.setString(4, temp[4]);
+		}
+		else
+			myStmt.setString(4, "%");
+
+		if(temp[5]!="*") {
+			myStmt.setString(5, temp[5]);
+		}
+		else
+			myStmt.setString(5, "%");
+
+		//TODO  delete statement
+		System.out.println(myStmt);
+		ResultSet myRs=myStmt.executeQuery();
+		
 		
 		while (myRs.next()) {
 			
 			sendBack += "property number: "+myRs.getInt("property number")+ ", listing price: " + myRs.getInt("list price")
-					+ ", cityQuadrent: " + myRs.getString("cityQuadrent")+", propertyType: " + myRs.getString("propertyType")+""
+					+ ", cityQuadrent: " + myRs.getString("cityQuadrent")+", propertyType: " + myRs.getString("propertyType")
 							+ ", nomBedroom: " + myRs.getString("nomBedroom")+", nomBathroom: " + myRs.getString("nomBathroom")+
 							", furnished: " + myRs.getString("furnished")+" \0";
 			
 		
 		}
-		
+		System.out.println(sendBack);
 		
 		return sendBack;
 	}
