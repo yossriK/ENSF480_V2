f76a7fa67d61ebe375fbbcd72e4bed716cb6e0eb
yossriK <46327024+yossriK@users.noreply.github.com>
2019-11-23 13:33:00 -0700

    Update Person.java

--- server/model/Person.java
+++ server/model/Person.java
@@ -1,17 +1,18 @@
 package server.model;
 
 public  class Person {
-
+	private int personID;
 	private Name name;
 	private Address addy;
 	private BirthDate BD;
 	
 	
-	Person(Name n,Address ad, BirthDate bd){
+	Person(Name n,Address ad, BirthDate bd, int id){
 		
 		this.name=n;
 		this.addy=ad;
 		this.BD=bd;
+		personID=id;
 	}
 
 
